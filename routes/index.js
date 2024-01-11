b519f11e6f2cc9a8713d7d8c25d5eb680e80a815
marefpceo <marefpceo@gmail.com>
2024-01-11 10:02:44 -0800

    Created controllers and routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Library Tutorial' });
+  res.redirect('/catalog');
 });
 
 module.exports = router;
