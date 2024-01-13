b83cc40f85e967b55dffd61a0868ce79a987a3f3
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-01-12 16:59:28 -0800

    Created author list page

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -14,14 +15,21 @@ AuthorSchema.virtual('name').get(function () {
   let fullname = '';
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
-  }w
-
+  }
   return fullname;
 });
 
 // Virtual author's URL
 AuthorSchema.virtual('url').get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 // Export model
