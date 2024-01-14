ce05d8106f2d56c70ef488c0435edd1ff14ec2ce
marefpceo <marefpceo@gmail.com>
2024-01-14 15:51:32 -0800

    Completed debugging of info not displaying
    Found a few typos on various pages. Main issue was translating
    a href tags from Pug JS syntax to EJS JS. Once corrected, all pages
    display the reqiured info up to the current point.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -37,27 +37,15 @@ exports.book_list = asyncHandler(async (req, res, next) => {
     .populate('author')
     .exec();
   
-  res.render('book_list', { title: 'Book List', book_list: allBooks });
+    res.render('book_list', { 
+      title: 'Book List', 
+      book_list: allBooks 
+    });
 });
 
 // Display detail page for a specific book.
 exports.book_detail = asyncHandler(async (req, res, next) => {
-  const [book, bookInstances] = await Promise.all([
-    Book.findById(req.params.id).populate('author').populate('genre').exec(),
-    BookInstance.find({ book: req.params.id }).exec(),
-  ]);
-
-  if (book === null) {
-    const  err = new Error('Book not found');
-    err.status = 404;
-    return next(err);
-  }
-
-  res.render('book_detail', {
-    title: book.title,
-    book: book,
-    book_instances: bookInstances,
-  });
+  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
 });
 
 // Display book create form on GET.
